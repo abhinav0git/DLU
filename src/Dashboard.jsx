fcd99f06537f507f51066cd3ea04325ef244c9ff
Abhinav <abhinav07c@gmail.com>
2024-04-07 23:25:50 +0530

    auth page ui

---
+++ src/Dashboard.jsx
@@ -0,0 +1,82 @@
+// import Link from "next/link"
+import React from 'react';
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import theLogo from '/src/media/cat-unsplash.jpg';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+// import { Link } from 'react-router-dom';
+
+
+const MyImage = () => (
+    <LazyLoadImage
+        alt="logo"
+        effect="blur"
+        wrapperProps={{
+            style: { transitionDelay: "1s" },
+        }}
+        src={theLogo} />
+);
+
+export function Dashboard() {
+    return (
+        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+            <div className="flex items-center justify-center py-12">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <div className="grid gap-2 text-center">
+                        <h1 className="text-3xl font-bold">Login</h1>
+                        <p className="text-balance text-muted-foreground">
+                            Enter your email below to login to your account
+                        </p>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                {/* <Link
+                                    href="/forgot-password"
+                                    className="ml-auto inline-block text-sm underline"
+                                >
+                                    Forgot your password?
+                                </Link> */}
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </div>
+                    <div className="mt-4 text-center text-sm">
+                        Don&apos;t have an account?{" "}
+                        <a href="./Signup.jsx" className='underline'>
+                            Sign Up
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden bg-muted lg:block">
+                <img className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                    style={{
+                        maxWidth: '1920',
+                        height: '1080',
+                    }}
+                    src={theLogo}
+                    alt="Logo"
+                />
+            </div>
+        </div>
+    )
+}
